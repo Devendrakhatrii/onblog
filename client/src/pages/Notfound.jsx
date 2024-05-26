e47fc0cf03d4a02822fb0643dbc562e8a4b811aa
Devendra khatri <devendrakhatri904@gmail.com>
2024-05-26 18:54:38 +0545

    Added 404 page and route

--- client/src/pages/Notfound.jsx
+++ client/src/pages/Notfound.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Notfound = () => {
-  return <div>Notfound</div>;
+  return (
+    <div>
+      <h1>404 page not found!</h1>
+      <Link to={"/"}>Home</Link>
+    </div>
+  );
 };
