c24177fc0671bff69f2fb4ff7e168965a8117a00
Devendra <devendrakhatri904@gmail.com>
2024-07-10 10:03:32 +0545

    completed admin navbar sticky

--- client/src/layouts/AdminNavbar.jsx
+++ client/src/layouts/AdminNavbar.jsx
@@ -36,78 +36,80 @@ export default function AdminNavbar() {
   }
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="h-6 w-6" />
-              <span className="">On Blog</span>
-            </Link>
-            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <NavLink
-                to="home"
-                className={({ isActive }) =>
-                  `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
-                    isActive ? "bg-muted text-primary" : null
-                  }`
-                }
-              >
-                <Home className="h-4 w-4" />
-                Home
-              </NavLink>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] ">
+      <div className="hidden border-r bg-muted/40 md:block  ">
+        <div className=" fixed w-1/6 h-full pl-3">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2 font-semibold">
+                <Package2 className="h-6 w-6" />
+                <span className="">On Blog</span>
+              </Link>
+              <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+                <Bell className="h-4 w-4" />
+                <span className="sr-only">Toggle notifications</span>
+              </Button>
+            </div>
+            <div className="flex-1">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                <NavLink
+                  to="home"
+                  className={({ isActive }) =>
+                    `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                      isActive ? "bg-muted text-primary" : null
+                    }`
+                  }
+                >
+                  <Home className="h-4 w-4" />
+                  Home
+                </NavLink>
 
-              <NavLink
-                to="blogs"
-                className={({ isActive }) =>
-                  `flex items-center gap-3 rounded-lg px-3 py-2  transition-all text-muted-foreground hover:text-primary ${
-                    isActive ? "bg-muted text-primary" : null
-                  }`
-                }
-              >
-                <Package className="h-4 w-4" />
-                Blogs{" "}
-              </NavLink>
-              <NavLink
-                to="users"
-                className={({ isActive }) =>
-                  `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
-                    isActive ? "bg-muted text-primary" : null
-                  }`
-                }
-              >
-                <Users className="h-4 w-4" />
-                Users
-              </NavLink>
-            </nav>
-          </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Go to Home</CardTitle>
-                <CardDescription>
-                  get back to home page for related information.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Link to={"/"}>
-                  <Button size="sm" className="w-full">
-                    Home
-                  </Button>
-                </Link>
-              </CardContent>
-            </Card>
+                <NavLink
+                  to="blogs"
+                  className={({ isActive }) =>
+                    `flex items-center gap-3 rounded-lg px-3 py-2  transition-all text-muted-foreground hover:text-primary ${
+                      isActive ? "bg-muted text-primary" : null
+                    }`
+                  }
+                >
+                  <Package className="h-4 w-4" />
+                  Blogs{" "}
+                </NavLink>
+                <NavLink
+                  to="users"
+                  className={({ isActive }) =>
+                    `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                      isActive ? "bg-muted text-primary" : null
+                    }`
+                  }
+                >
+                  <Users className="h-4 w-4" />
+                  Users
+                </NavLink>
+              </nav>
+            </div>
+            <div className="mt-auto p-4">
+              <Card x-chunk="dashboard-02-chunk-0">
+                <CardHeader className="p-2 pt-0 md:p-4">
+                  <CardTitle>Go to Home</CardTitle>
+                  <CardDescription>
+                    get back to home page for related information.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                  <Link to={"/"}>
+                    <Button size="sm" className="w-full">
+                      Home
+                    </Button>
+                  </Link>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 sticky top-0 z-50">
           <Sheet>
             <SheetTrigger asChild>
               <Button
