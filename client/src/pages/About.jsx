a14e5fab0be6975493d6fe95e2e7a07b9580ef0f
Devendra <devendrakhatri904@gmail.com>
2024-07-11 18:37:56 +0545

    Completed working on about and contact page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,71 @@
+import {
+  DiscordLogoIcon,
+  InstagramLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from "@radix-ui/react-icons";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className=" px-10  min-h-[100vh]  w-full">
+      <div className=" h-[30vh] flex items-center justify-center flex-col text-center">
+        <h1 className="font-bold text-7xl">About Us.</h1>
+        <div className=" px-20 mx-40 mt-5">
+          <p>
+            At [on Blog], we believe that everyone has a story to tell and a
+            unique perspective to share. Our mission is to provide a platform
+            where voices can be heard, ideas can be exchanged, and communities
+            can be built.
+          </p>
+        </div>
+      </div>
+      <div className=" h-[50vh] flex gap-5">
+        <div className="w-1/2 h-full  flex items-center justify-center">
+          <h1 className="font-extrabold text-4xl">"What are we all About?"</h1>
+        </div>
+        <div className="w-1/2 p-10  flex gap-4 flex-col font-semibold">
+          <p>
+            Our mission is to empower individuals to share their stories and
+            connect with others. We aim to create a welcoming and inclusive
+            space where diversity is celebrated, and meaningful conversations
+            can flourish.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            necessitatibus reiciendis officiis quo ex tempore mollitia animi
+            reprehenderit. Vero ullam, assumenda fugit possimus labore iste
+            obcaecati reiciendis hic recusandae facilis, nemo unde neque
+            pariatur esse autem commodi itaque repellendus. Odio, asperiores.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            fugiat tempora adipisci dolores temporibus animi eligendi impedit
+            ducimus quam illum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            fugiat tempora adipisci dolores temporibus animi eligendi impedit
+            ducimus quam illum.
+          </p>
+        </div>
+      </div>
+      <div className=" h-[50vh] w-full flex items-center justify-center text-center pt-16">
+        <div className="h-full w-1/2">
+          <h1 className="font-extrabold text-4xl">
+            Find more about us through.
+          </h1>
+
+          <div className="flex items-center justify-evenly mt-20  gap-2 h-20 transition-transform">
+            <InstagramLogoIcon className="h-20 w-20 hover:h-24 hover:w-24 cursor-pointer transition-transform" />
+
+            <LinkedInLogoIcon className="h-20 w-20 hover:h-24 hover:w-24 cursor-pointer transition-transform" />
+            <TwitterLogoIcon className="h-20 w-20 hover:h-24 hover:w-24 cursor-pointer transition-transform" />
+            <DiscordLogoIcon className="h-20 w-20 hover:h-24 hover:w-24 cursor-pointer transition-transform " />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,70 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { MailIcon, MapPin, Phone } from "lucide-react";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className=" px-10  min-h-[100vh]  w-full">
+      <div className=" h-[30vh] flex items-center justify-center flex-col text-center">
+        <h1 className="font-bold text-7xl">Contact Us.</h1>
+        <div className=" px-20 mx-40 mt-5">
+          <p>
+            We are constantly evolving and looking for ways to improve. Your
+            feedback is invaluable to us, and we welcome any suggestions you may
+            have. Thank you for being a part of our community and for sharing
+            your journey with us.
+          </p>
+        </div>
+      </div>
+      <div className=" h-[50vh] flex gap-5">
+        <div className="w-1/2 h-full  flex items-center justify-center">
+          <h1 className="font-extrabold text-4xl">
+            "Fell free to share your opinion?"
+          </h1>
+        </div>
+        <div className="w-1/2 p-10 px-40   flex gap-4 flex-col font-semibold">
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" type="text" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" required />
+          </div>
+          <div className="grid  gap-2">
+            <Label htmlFor="message">Your message</Label>
+            <Textarea placeholder="Type your message here." id="message" />
+          </div>
+          <div className="flex  justify-end ">
+            <Button type="submit" variant={"outline"}>
+              Send
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className=" h-[50vh] w-full flex items-center justify-center text-center pt-16">
+        <div className="h-full w-1/2">
+          <h1 className="font-extrabold text-4xl">Info.</h1>
+
+          <div className="flex  items-center justify-center p-5 m-10 gap-10 ">
+            <div className="  flex flex-col items-center justify-center">
+              <MailIcon className="h-20 w-20" />
+              <h1>support@onblog.com</h1>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <MapPin className="h-20 w-20" />
+              <h1>Kathmandu, Nepal</h1>
+            </div>
+            <div className=" flex flex-col items-center justify-center">
+              <Phone className="h-20 w-20" />
+              <h1>+977 9762961214</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
