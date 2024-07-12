7f5cf834161dce0c633276faf47c6bd82e702a78
Devendra <devendrakhatri904@gmail.com>
2024-07-12 14:01:37 +0545

    completed landingpage mobile responsiveness

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -13,23 +13,28 @@ const LandingPage = () => {
   });
   return (
     <div className=" h-screen w-full">
-      <div className=" h-[10vh] flex w-full items-center justify-between px-20">
-        <h1 className="font-extrabold text-4xl cursor-pointer">On Blog.</h1>
-        <div className=" p-5 w-1/4 flex justify-evenly gap-5">
+      <div className=" h-[10vh] flex w-full items-center justify-between md:px-20 px-6">
+        <h1 className="font-extrabold md:text-4xl text-3xl cursor-pointer">
+          On Blog.
+        </h1>
+        <div className=" md:p-5 md:w-1/4 flex justify-evenly md:gap-5 gap-2">
           <Link to={"/login"}>
-            <Button variant={"outline"} className="h-10 px-8 rounded-full">
+            <Button
+              variant={"outline"}
+              className="md:h-10 md:px-8 rounded-full"
+            >
               Login
             </Button>
           </Link>
           <Link to={"/register"}>
-            <Button variant="outline" className="h-10 px-8 rounded-full">
+            <Button variant="outline" className="md:h-10  md:px-8 rounded-full">
               Register
             </Button>
           </Link>
         </div>
       </div>
 
-      <div className="h-[90vh]  flex items-start justify-start px-20 pt-40 flex-col gap-6">
+      <div className="h-[90vh]  flex items-start justify-start md:px-20 px-6 pt-40 flex-col gap-6">
         <h1 className="font-extrabold text-7xl">
           "Blog Freely, <br />
           Connect Deeply"
@@ -38,7 +43,7 @@ const LandingPage = () => {
           Your Stories, Your Voice, Your Platform
         </h1>
         <Link to={"/users/home"}>
-          <Button className="h-12 rounded-full p-10 py-8 text-2xl">
+          <Button className="h-12 rounded-full p-10 py-8 text-2xl mt-5">
             Get Started
           </Button>
         </Link>
